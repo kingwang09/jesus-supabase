32e21bd681a172a86a2827ae22899e3a96dcd138
kingwang09 <kingwang09@gmail.com>
2024-02-08 14:32:01 +0900

    리팩토링

--- src/price/interfaces/price.interface.ts
+++ src/price/interfaces/price.interface.ts
@@ -1,9 +1,9 @@
 import { Json } from "../../../supabase/type/database.types";
 
 interface IPriceByDaily {
-    date: string,
-    price: number,
-    priceHistory: number[],
+    date: string,//날짜별
+    price: number,//최근 수집된 가격
+    priceHistory: number[],//같은날 이력
     lastCrawledAt: Date,
 }
 
--- src/price/upsert.ts
+++ src/price/upsert.ts
@@ -9,8 +9,105 @@ dotenv.config();
 
 const SUPABASE_URL = process.env.SUPABASE_URL || '';
 const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY || '';
+const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_API_KEY);
 
-const dummyCrawlingPayload: CrawlingPayload = {
+const main = async (crawlingPayload: CrawlingPayload, productMeta: CrawledProductMeta) => {
+    //크롤링 이력 쌓기 시작 -> mall_type & categoryKey & model
+    const { data, error } = await supabase
+        .from('simple_affiliate_links_history')
+        .select("*")
+        .eq('mall_type', productMeta.mallType)
+        .eq('product_category_key', crawlingPayload.productCategoryKey)
+        .eq('model', crawlingPayload.modelName)
+        .single();
+    console.log('select one: ', data);
+
+
+    //mall_type & categoryKey & model 데이터가 존재하는 경우,
+    if(data){
+        //오늘 날짜 price_by_daily[] 찾아서 
+        await updatePriceByDaily(data.price_by_daily as unknown as IPriceByDaily[], crawlingPayload, productMeta);
+    }else{//없는 경우
+        console.log('데이터 없음');
+        await insertPriceHistoryByDaily(crawlingPayload, productMeta);
+    }
+};
+const updatePriceByDaily = async (priceListByDaily: IPriceByDaily[], crawlingPayload: CrawlingPayload, productMeta: CrawledProductMeta) => {
+    let _priceListByDaily = priceListByDaily;//data.price_by_daily as unknown as IPriceByDaily[];
+    console.log('priceByDaily: ', priceListByDaily);
+    const currentDateKey = formatDate();
+    const dateKeyIndex = _.findIndex(priceListByDaily, {'date':currentDateKey});
+    console.log('dateKeyIndex: ', dateKeyIndex);
+
+    
+    if(dateKeyIndex != -1){//이미 동일한 날짜로 수집이 된 경우
+        const updatePriceByDaily = getUpdatePriceByDaily(dateKeyIndex, _priceListByDaily, productMeta.price);
+        
+        //최신 수집된 가격으로 날짜 배열 교체
+        _priceListByDaily.splice(dateKeyIndex, 1, updatePriceByDaily);
+        console.log('priceListByDaily: ', _priceListByDaily);
+    }else{//새로 수집된 경우
+        const insertPriceByDaily: IPriceByDaily = {
+            date: currentDateKey,
+            price: productMeta.price,
+            priceHistory: [productMeta.price],
+            lastCrawledAt: new Date(),
+        }
+        //최신 수집된 가격으로 날짜 배열 추가
+        _priceListByDaily.push(insertPriceByDaily);
+    }
+
+
+    await supabase.from('simple_affiliate_links_history')
+        .update({
+            link: productMeta.link,
+            image_url: productMeta.imageUrl,
+
+            price_by_last_crawled: productMeta.price,
+            price_by_daily: _priceListByDaily as unknown as Json,
+        })
+        .eq('model', crawlingPayload.modelName)
+        .eq('product_category_key', crawlingPayload.productCategoryKey)
+        .eq('mall_type', productMeta.mallType)
+};
+
+const getUpdatePriceByDaily = (dateKeyIndex: number, priceListByDaily: IPriceByDaily[], latestPrice: number): IPriceByDaily => {
+    const priceByDaily = priceListByDaily[dateKeyIndex];
+    priceByDaily.priceHistory.push(latestPrice);
+
+    const updatePriceByDaily: IPriceByDaily = {
+        date: priceByDaily.date,
+        price: latestPrice,
+        priceHistory: priceByDaily.priceHistory,
+        lastCrawledAt: new Date(),
+    }
+    console.log('[getUpdatePriceByDaily] updatePriceByDaily=', updatePriceByDaily);
+    return updatePriceByDaily;
+} ;
+
+const insertPriceHistoryByDaily = async (crawlingPayload: CrawlingPayload, productMeta: CrawledProductMeta) => {
+    const firstPriceByDaily: IPriceByDaily = {
+        date: formatDate(),
+        price: productMeta.price,
+        priceHistory: [productMeta.price],
+        lastCrawledAt: new Date(),
+    };
+    await supabase.from('simple_affiliate_links_history')
+        .insert({
+            mall_type: productMeta.mallType,
+            product_category_key: crawlingPayload.productCategoryKey,
+            model: crawlingPayload.modelName,
+            brand: crawlingPayload.brand,
+
+            link: productMeta.link,
+            image_url: productMeta.imageUrl,
+            price_by_last_crawled: productMeta.price,
+            price_by_daily: [firstPriceByDaily] as unknown as Json,
+        });
+};
+
+//price_by_daily
+const dummyCrawlingPayload1: CrawlingPayload = {
     danawaUrl: '',
     mallType: CrawlingMallType.ALL,
     productId: 1,
@@ -21,109 +118,11 @@ const dummyCrawlingPayload: CrawlingPayload = {
     basePrice: 150000,
     retryCount: 1,
 };
-const dummyProductMeta: CrawledProductMeta = {
+const dummyProductMeta1: CrawledProductMeta = {
     price: 1950000,
     link: '쿠팡상세링크',
     productTitle: '삼성 이재용 무죄 판결 헌정 모델',
     imageUrl: '이미지링크',
     mallType: CrawlingMallType.COUPANG,
 };
-
-const main = async () => {
-    const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_API_KEY);
-
-    const { data, error } = await supabase
-        .from('simple_affiliate_links_history')
-        .select("*")
-        .eq('model', dummyCrawlingPayload.modelName)
-        .eq('product_category_key', dummyCrawlingPayload.productCategoryKey)
-        .eq('mall_type', dummyProductMeta.mallType)
-        .single();
-    console.log('data: ', data);
-
-
-    if(data){//데이터가 존재하는 경우
-        console.log('데이터 있음');
-
-        //오늘 날짜 price_by_daily[] 찾아서 
-        let priceListByDaily = data.price_by_daily as unknown as IPriceByDaily[];
-        console.log('priceByDaily: ', priceListByDaily);
-        const currentDateKey = formatDate();
-        const dateKeyIndex = _.findIndex(priceListByDaily, {'date':currentDateKey});
-        console.log('dateKeyIndex: ', dateKeyIndex);
-
-        
-        if(dateKeyIndex != -1){//이미 동일한 날짜로 수집이 된 경우
-            const priceByDaily = priceListByDaily[dateKeyIndex];
-            priceByDaily.priceHistory.push(dummyProductMeta.price);
-
-            const updatePriceByDaily: IPriceByDaily = {
-                date: priceByDaily.date,
-                price: dummyProductMeta.price,
-                priceHistory: priceByDaily.priceHistory,
-                lastCrawledAt: new Date(),
-            }
-            console.log('updatePriceByDaily: ', updatePriceByDaily);
-
-            //배열 교체
-            priceListByDaily.splice(dateKeyIndex, 1, updatePriceByDaily);
-            console.log('priceListByDaily: ', priceListByDaily);
-        }else{//새로 수집된 경우
-            const insertPriceByDaily: IPriceByDaily = {
-                date: currentDateKey,
-                price: dummyProductMeta.price,
-                priceHistory: [dummyProductMeta.price],
-                lastCrawledAt: new Date(),
-            }
-            priceListByDaily.push(insertPriceByDaily);
-        }
-        
-        //date-key로 추출
-        
-        //있으면 
-            //update price 
-            //update price History push
-            //update lastCrawledAt new Date()
-        //없으면
-            //insert price
-            //insert price history
-            //insert lastCrawledAt new Date()
-
-        await supabase.from('simple_affiliate_links_history')
-            .update({
-                link: dummyProductMeta.link,
-                image_url: dummyProductMeta.imageUrl,
-
-                price_by_last_crawled: dummyProductMeta.price,
-                price_by_daily: priceListByDaily as unknown as Json,
-            })
-            .eq('model', dummyCrawlingPayload.modelName)
-            .eq('product_category_key', dummyCrawlingPayload.productCategoryKey)
-            .eq('mall_type', dummyProductMeta.mallType)
-
-    }else{//없는 경우
-        console.log('데이터 없음');
-        const firstPriceByDaily: IPriceByDaily = {
-            date: formatDate(),
-            price: dummyProductMeta.price,
-            priceHistory: [dummyProductMeta.price],
-            lastCrawledAt: new Date(),
-        };
-        await supabase.from('simple_affiliate_links_history')
-            .insert({
-                mall_type: dummyProductMeta.mallType,
-                product_category_key: dummyCrawlingPayload.productCategoryKey,
-                model: dummyCrawlingPayload.modelName,
-                brand: dummyCrawlingPayload.brand,
-
-                link: dummyProductMeta.link,
-                image_url: dummyProductMeta.imageUrl,
-                price_by_last_crawled: dummyProductMeta.price,
-                price_by_daily: [firstPriceByDaily] as unknown as Json,
-            });
-    }
-};
-
-//price_by_daily
-
-main();
+main(dummyCrawlingPayload1, dummyProductMeta1,);
