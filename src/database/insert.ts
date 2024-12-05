d466623713678d46d69a53acafda18128d32a544
kingwang09 <kingwang09@gmail.com>
2024-12-05 12:39:53 +0900

    날짜 처리 로직 개선

--- src/database/insert.ts
+++ src/database/insert.ts
@@ -1,8 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
-import { Database } from '../../supabase/database.types';
 import dotenv from 'dotenv';
-
 dotenv.config();
+import { createClient } from '@supabase/supabase-js';
+import moment from 'moment-timezone';
+import { Database } from '../../supabase/type/database.types';
+
+
 
 const SUPABASE_URL = process.env.SUPABASE_URL || '';
 const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY || '';
@@ -10,15 +12,22 @@ const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_API_KEY);
 
 const main = async () => {
     console.log('hello');
-    const { error } = await supabase.from('User').insert({
-        email: 'leeyeojeong@naver.com',
-        name: 'lee',
-    });
-    if(error){
-        console.error('insert error: ', error);
-    }else{
-        console.log('insert success.');
-    }
+    const expiredAt = moment().add(1, 'days').toDate().toISOString();
+    console.log('test: ', expiredAt);
+    const kstDate = moment(expiredAt).tz('Asia/Seoul');
+    console.log(kstDate.format());
+    // const { error } = await supabase.from('user_membership').insert({
+    //     expiredAt: expiredAt,
+    //     status: 'completed',
+    //     payment: {},
+    //     membershipId: 1,
+    //     userId: 1,
+    // });
+    // if(error){
+    //     console.error('insert error: ', error);
+    // }else{
+    //     console.log('insert success.');
+    // }
 };
 
 main();
