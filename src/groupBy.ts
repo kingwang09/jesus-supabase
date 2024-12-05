f2064a0089bf57b5518d756748ed8f27f0791d63
kingwang09 <kingwang09@gmail.com>
2024-12-05 12:35:57 +0900

    기본 조회 기능구현

---
+++ src/groupBy.ts
@@ -0,0 +1,24 @@
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import { Database } from '../supabase/type/database.types';
+dotenv.config();
+
+const SUPABASE_URL = process.env.SUPABASE_URL || '';
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY || '';
+
+const main = async () => {
+    console.log('hello');
+    const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_API_KEY);
+
+    const { data, error } = await supabase
+        .from('User')
+        .select('email');
+    console.log('data: ', data);
+    if(data){
+        for(const element of data){
+            console.log(`email: ${element.email}`);
+        }
+    }
+};
+
+main();
